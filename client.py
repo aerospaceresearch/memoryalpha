ba67639edbc6a7c5d731061be57e56390be04b34
hornig <chefkueche@hornshouse.com>
2017-08-05 21:39:37 +0200

    client and server do authentification now. several users

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
 files = {'file': open('main.py', 'rb')}
 
-r = requests.post('http://localhost:5000', files=files)
+r = requests.post('http://localhost:5000', files=files, auth=HTTPBasicAuth('admin1', 'secret1'))
 print(r.status_code)
--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 from flask import Flask
 import os
 import uuid
-from flask import Flask, request, redirect, flash, render_template
+from flask import Flask, request, redirect, flash, render_template, Response
 from werkzeug.utils import secure_filename
+from functools import wraps
 
 app = Flask(__name__)
 app.secret_key = "SpaceRocks1!" # for the cookie
 
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+
+    # checking for users
+    for uname, pword in (('admin1', 'secret1'),('admin2', 'secret2')):
+        if uname == username and pword == password:
+            return True
+
+    return False
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 @app.route('/', methods=['GET', 'POST'])
+@requires_auth
 def upload_file():
     if request.method == 'POST':
 
